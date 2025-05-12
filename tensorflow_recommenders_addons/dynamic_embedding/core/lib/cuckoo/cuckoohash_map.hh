4b6e2b23cfbab5334246fd525ce08f0413734548
Jatin Mandav <jatin@sharechat.co>
2025-05-12 23:31:50 +0530

    udpate

--- tensorflow_recommenders_addons/dynamic_embedding/core/lib/cuckoo/cuckoohash_map.hh
+++ tensorflow_recommenders_addons/dynamic_embedding/core/lib/cuckoo/cuckoohash_map.hh
@@ -363,7 +363,17 @@ class cuckoohash_map {
       return 0;
     }
     counter_type s = 0;
+    int num_prints = 0;
     for (spinlock &lock : get_current_locks()) {
+      if (!lock.is_migrated()) {
+        while(1) {
+          if (num_prints > 5) {
+              std::cout << "Lock not migrated!" << std::endl;
+          }
+          num_prints++;
+        }
+      }
+      num_prints = 0;
       s += lock.elem_counter();
     }
     assert(s >= 0);
