20069ad548d63a4b13d650b323c353290e6a2da7
Jatin Mandav <jatin@sharechat.co>
2025-05-12 20:53:23 +0530

    udpate

--- tensorflow_recommenders_addons/dynamic_embedding/core/lib/cuckoo/cuckoohash_map.hh
+++ tensorflow_recommenders_addons/dynamic_embedding/core/lib/cuckoo/cuckoohash_map.hh
@@ -366,7 +366,7 @@ class cuckoohash_map {
     std::cout << "Size call: " << std::endl;
     for (spinlock &lock : get_current_locks()) {
       s += lock.elem_counter();
-      std:cout << "  Size loop: " << s << " elem-counter: " << lock.elem_counter() << std::endl;
+      std::cout << "  Size loop: " << s << " elem-counter: " << lock.elem_counter() << std::endl;
     }
     assert(s >= 0);
     return static_cast<size_type>(s);
