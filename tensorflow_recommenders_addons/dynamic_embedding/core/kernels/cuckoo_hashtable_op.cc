9824be7f853e861b9ad454be974421555ab47e3d
Jatin Mandav <jatin@sharechat.co>
2025-05-12 15:42:01 +0530

    udpate

--- tensorflow_recommenders_addons/dynamic_embedding/core/kernels/cuckoo_hashtable_op.cc
+++ tensorflow_recommenders_addons/dynamic_embedding/core/kernels/cuckoo_hashtable_op.cc
@@ -348,11 +348,11 @@ class CuckooHashTableOfTensors final : public LookupInterface {
     const size_t table_size = table_->size();
     size_t search_offset = 0;
     size_t total_saved = 0;
-    cout << "table size: " << table_->size() << " for: " << filepath << endl;
+    std::cout << "table size: " << table_->size() << " for: " << filepath << std::endl;
     while (search_offset < table_size) {
       auto dump_counter = table_->dump((K*)key_buffer, (V*)value_buffer,
                                        search_offset, buffer_size);
-      count << "    dump_counter: " << dump_counter << endl;
+      std::count << "    dump_counter: " << dump_counter << std::endl;
       search_offset += dump_counter;
       key_offset += dump_counter * sizeof(K);
       value_offset += dump_counter * value_len;
