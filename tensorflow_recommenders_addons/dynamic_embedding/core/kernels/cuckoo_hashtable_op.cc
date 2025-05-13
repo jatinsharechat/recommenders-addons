1f2532705a7fc7a3dca659f46226902adf7054fa
Jatin Mandav <jatin@sharechat.co>
2025-05-13 18:12:01 +0530

    udpate

--- tensorflow_recommenders_addons/dynamic_embedding/core/kernels/cuckoo_hashtable_op.cc
+++ tensorflow_recommenders_addons/dynamic_embedding/core/kernels/cuckoo_hashtable_op.cc
@@ -123,7 +123,7 @@ struct LaunchTensorsInsert<CPUDevice, K, V> {
 //        table->insert_or_assign(key_flat(i), value_flat, value_dim_, i);
 //      }
 //    };
-    std::cout << "table size accum call:" << table->size() << " -- keys: " << total << " -- values: " << value_flat.size() << std::endl;
+    std::cout << "table size insert call:" << table->size() << " -- keys: " << total << " -- values: " << value_flat.size() << std::endl;
     auto shard = [this, &table, key_flat, &value_flat](int64 begin, int64 end) {
       for (int64 i = begin; i < end; ++i) {
         table->insert_or_assign(key_flat(i), value_flat, value_dim_, i);
